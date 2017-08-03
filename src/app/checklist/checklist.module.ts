9974fdac479ccdd409fdc4d1926da9641e1a535c
Guaratã Alencar <guara_st@hotmail.com>
2017-08-02 22:05:59 -0300

    checklist service

--- src/app/checklist/checklist.module.ts
+++ src/app/checklist/checklist.module.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { ChecklistComponent } from 'app/checklist/checklist.component';
+import { ChecklistService } from 'app/checklist/checklist.service';
 import { ComponentsModule } from 'app/lib/components/components.module';
 import { PanelComponent } from 'app/checklist/panel/panel.component';
 import { TaskItemComponent } from 'app/checklist/task/task-item/task-item.component';
@@ -22,6 +23,9 @@ import { TaskListComponent } from 'app/checklist/task/task-list/task-list.compon
   ],
   exports: [
     ChecklistComponent
+  ],
+  providers: [
+    ChecklistService
   ]
 })
 export class ChecklistModule { }
