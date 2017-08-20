b4b49b686e9ac2472cdba83d41500b6e3b483971
Guaratã Alencar <guara_st@hotmail.com>
2017-08-20 16:21:47 -0300

    basic login with google

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,14 @@ import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { environment } from '../environments/environment';
-import { HomeModule } from 'app/home/home.module';
+import { AuthModule } from 'app/auth/auth.module';
 
 @NgModule({
   imports: [
     BrowserModule,
-    HomeModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    AuthModule
   ],
   declarations: [
     AppComponent
