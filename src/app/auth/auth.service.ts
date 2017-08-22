201297005a7ce787b2546625d21577dca22dbd2e
Guaratã Alencar <guara_st@hotmail.com>
2017-08-21 22:09:30 -0300

    remove firebase ui

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -9,25 +9,8 @@ export class AuthService {
 
   private authState: Observable<firebase.User>
   private currentUser: firebase.User = null;
-  public fui_auth;
-  public uiConfig = {
-    signInSuccessUrl: '<url-to-redirect-to-on-success>',
-    signInOptions: [
-      // Leave the lines as is for the providers you want to offer your users.
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      firebase.auth.PhoneAuthProvider.PROVIDER_ID
-    ],
-    // Terms of service url.
-    tosUrl: '<your-tos-url>'
-  };
 
   constructor(public afAuth: AngularFireAuth) {
-    this.fui_auth = new firebaseui.auth.AuthUI(this.afAuth.auth);
-
     this.authState = this.afAuth.authState;
     this.authState.subscribe(user => {
       if (user) {
