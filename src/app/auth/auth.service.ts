7387b22eee31987b7de02ad844b82e7828f45bed
Guaratã Alencar <guara_st@hotmail.com>
2017-08-21 22:05:27 -0300

    bug-fix

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
-import * as firebaseui from 'firebaseui';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
