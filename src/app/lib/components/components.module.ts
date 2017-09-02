361bc28f37f12634d2238f6d947377ca404c92fa
Guaratã Alencar <guara_st@hotmail.com>
2017-09-02 18:48:05 -0300

    basic form settings

--- src/app/lib/components/components.module.ts
+++ src/app/lib/components/components.module.ts
@@ -1,34 +1,41 @@
+import { FormQuestionComponent } from './form/form-question/form-question.component';
+import { FormComponent } from './form/form.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormEditComponent } from 'app/lib/components/form-edit/Form-edit.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoadingComponent } from './loading/loading.component';
 // Material
 import { MdButtonModule } from '@angular/material';
 import { MdCardModule } from '@angular/material';
 import { MdToolbarModule } from '@angular/material';
 import { MdMenuModule } from '@angular/material';
+import { MdInputModule } from '@angular/material';
 
 const components = [
   FormEditComponent,
-  LoadingComponent
+  LoadingComponent,
+  FormComponent,
 ];
 
 const materialModules = [
   MdButtonModule,
   MdCardModule,
   MdToolbarModule,
-  MdMenuModule
+  MdMenuModule,
+  MdInputModule
 ]
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     ...materialModules
   ],
   declarations: [
-    ...components
+    ...components,
+    FormQuestionComponent
   ],
   exports: [
     ...components,
---
+++ src/app/lib/components/form/form-question/form-question.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+import { QuestionBase } from './../question-base';
+
+@Component({
+  selector: 'df-form-question',
+  templateUrl: './form-question.component.html',
+  styleUrls: ['./form-question.component.css']
+})
+export class FormQuestionComponent {
+
+  @Input() question: QuestionBase<any>;
+  @Input() form: FormGroup;
+  get isValid() { return this.form.controls[this.question.key].valid; }
+
+}
